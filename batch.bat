f8445fc2ed01055f98b61832a8522c42d4d17459
pollarteon <prafulph04@gmail.com>
2024-10-08 18:19:12 +0530

    Stage 17 Completed

---
+++ batch.bat
@@ -0,0 +1,18 @@
+load --init ../expl/samples/shell.xsm
+load --int=10 ../spl/spl_progs/int_10.xsm
+load --exhandler ../spl/spl_progs/haltprog.xsm
+load --os ../spl/spl_progs/os_startup.xsm
+load --library ../expl/library.lib
+load --int=timer ../spl/spl_progs/sample_timer.xsm
+load --int=console ../spl/spl_progs/console_interrupt.xsm
+load --int=disk ../spl/spl_progs/disk_interrupt.xsm
+load --int=7 ../spl/spl_progs/int_7.xsm
+load --int=6 ../spl/spl_progs/int_6.xsm
+load --int=9 ../spl/spl_progs/int_9.xsm
+load --idle ../expl/samples/idleProcess.xsm
+load --module 7 ../spl/spl_progs/module_7.xsm
+load --module 5 ../spl/spl_progs/module_5.xsm
+load --module 0 ../spl/spl_progs/module_0.xsm
+load --module 4 ../spl/spl_progs/module_4.xsm
+load --module 1 ../spl/spl_progs/module_1.xsm
+load --module 2 ../spl/spl_progs/module_2.xsm
