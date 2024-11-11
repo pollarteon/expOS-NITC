2c1616b23f7a6e38ba3a4945eb9ddfb2d7914ad3
pollarteon <prafulph04@gmail.com>
2024-11-12 00:47:31 +0530

    stage 26 assgn not done

--- batch.bat
+++ batch.bat
@@ -1,4 +1,5 @@
-load --init ../expl/samples/shell2.xsm
+load --init ../expl/samples/login.xsm
+load --shell ../expl/samples/Shell_26.xsm
 load --int=10 ../spl/spl_progs/int_10.xsm
 load --exhandler ../spl/spl_progs/exception_handler.xsm
 load --os ../spl/spl_progs/os_startup.xsm
@@ -6,15 +7,19 @@ load --library ../expl/library.lib
 load --int=timer ../spl/spl_progs/sample_timer.xsm
 load --int=console ../spl/spl_progs/console_interrupt.xsm
 load --int=disk ../spl/spl_progs/disk_interrupt.xsm
+load --int=5 ../spl/spl_progs/int_5.xsm
 load --int=4 ../spl/spl_progs/int_4.xsm
 load --int=7 ../spl/spl_progs/int_7.xsm
 load --int=6 ../spl/spl_progs/int_6.xsm
 load --int=9 ../spl/spl_progs/int_9.xsm
 load --int=8 ../spl/spl_progs/int_8.xsm
 load --int=11 ../spl/spl_progs/int_11.xsm
+load --int=12 ../spl/spl_progs/int_12.xsm
 load --int=13 ../spl/spl_progs/int_13.xsm
 load --int=14 ../spl/spl_progs/int_14.xsm
 load --int=15 ../spl/spl_progs/int_15.xsm
+load --int=17 ../spl/spl_progs/int_17.xsm
+load --int=16 ../spl/spl_progs/int_16.xsm
 load --idle ../expl/samples/idleProcess.xsm
 load --module 7 ../spl/spl_progs/module_7.xsm
 load --module 5 ../spl/spl_progs/module_5.xsm
@@ -22,6 +27,7 @@ load --module 0 ../spl/spl_progs/module_0.xsm
 load --module 4 ../spl/spl_progs/module_4.xsm
 load --module 1 ../spl/spl_progs/module_1.xsm
 load --module 2 ../spl/spl_progs/module_2.xsm
+load --module 3 ../spl/spl_progs/module_3.xsm
 load --exec ../expl/samples/LL.xsm
 load --exec ../expl/samples/even100.xsm
 load --exec ../expl/samples/oddnos.xsm
@@ -36,3 +42,13 @@ load --exec ../expl/samples/child.xsm
 load --exec ../expl/samples/asg3_22.xsm
 load --exec ../expl/samples/create.xsm
 load --exec ../expl/samples/delete.xsm
+load --exec ../expl/samples/fileRead.xsm
+load --exec ../expl/samples/stage24.xsm
+load --exec ../expl/samples/f_write.xsm
+load --exec ../expl/samples/asg3_24.xsm
+load --exec ../expl/samples/asg4_25.xsm
+load --exec ../expl/samples/ls.xsm
+load --exec ../expl/samples/lu.xsm
+load --exec ../expl/samples/ru.xsm
+load --data ../expl/samples/mdata.dat
+load --data ../sample.dat
